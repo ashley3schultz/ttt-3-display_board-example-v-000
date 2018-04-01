7ea25ca22ab5a1e32cfe4572f6a61917d8ba7812
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-01 22:58:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,11 @@
 def dislay_board
+  puts "Welcome to Tic Tac Toe"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+  puts "what move would X like to make (1-9)?"
 end
 
 display_board
